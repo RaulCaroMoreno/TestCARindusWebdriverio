d81769fdb33b4f153e678cd56f64db67e9f31448
RaulCaroMoreno <lasent41@gmail.com>
2023-06-26 21:23:59 +0200

    refactor

--- packages/web/pageObjects/homePage.js
+++ packages/web/pageObjects/homePage.js
@@ -2,7 +2,6 @@ const PageBase = require('./pageBase');
 const myAccountFrame = require('./frame/myAccountFrame');
 
 class HomePage extends PageBase {
-
 	constructor() {
 		super();
 		this.accountFrame = myAccountFrame;
@@ -13,14 +12,14 @@ class HomePage extends PageBase {
 	}
 
 	get headlineWelcome() {
-		return $$("//span[@data-qa='Headline']").find( async (elem) => {
-			return await elem.isDisplayed();
-		});
+		return $$("//span[@data-qa='Headline']").find((elem) => elem.isDisplayed());
 	}
 
 	async acceptCookiesButton() {
 		await this.click(this.cookiesButton);
 	}
+
+	hole = e
 
 	async clickHeadline() {
 		await this.click(this.headlineWelcome);
@@ -28,15 +27,16 @@ class HomePage extends PageBase {
 
 	async openHomePage() {
 		await browser.url('https://www.c-and-a.com/es/es/shop');
-		if(await browser.isMobile === false) {
+		if ((await browser.isMobile) === false) {
 			await browser.maximizeWindow();
 		}
-		if(await this.elementIsShown(this.cookiesButton, {timeout:2500}) === true) {
+		if (
+			(await this.elementIsShown(this.cookiesButton, { timeout: 2500 })) ===
+			true
+		) {
 			await this.acceptCookiesButton();
 		}
 	}
-
-	
 }
 
 module.exports = new HomePage();
